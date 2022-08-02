abef5141fc30e822e6caab608bec62ea0f398eb5
larsjr <larsroine@gmail.com>
2022-08-02 19:36:48 +0200

    Add comment in keymap.lua

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -51,6 +51,8 @@ keymap("v", ">", ">gv", opts)
 -- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Keep yanked word when pasting over other word
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
